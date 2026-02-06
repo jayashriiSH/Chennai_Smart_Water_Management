a352c4ebdacfeecaea28b8be5c6abb7215edd71d
jayashriiSH <pcmj.jayashriishankar@gmail.com>
2026-02-06 19:45:34 +0530

    update
    Removed Supabase URL and key for security reasons.

--- test_supabase.py
+++ test_supabase.py
@@ -1,8 +1,8 @@
 from supabase import create_client
 
 supabase = create_client(
-    "https://dstilpwjehgsfgbuakjf.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzdGlscHdqZWhnc2ZnYnVha2pmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODc1NTA2NywiZXhwIjoyMDg0MzMxMDY3fQ.SNwYXUXV4GwFZFYAS4seKMaIXxKWcHlZMJabUTGKsc0"
+   
 )
 
 print("✅ Supabase connected successfully")
+
